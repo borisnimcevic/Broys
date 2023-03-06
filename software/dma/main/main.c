14baf2ecb5c13b73fb70fea31157c5bda94d38ca
Boris Nimcevic <borisnimcevic@gmail.com>
2023-03-06 12:49:19 +0100

    added functions for setting the digit and the place

--- software/dma/main/main.c
+++ software/dma/main/main.c
@@ -1,16 +1,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "esp_async_memcpy.h"
 #include "esp_log.h"
-#include "esp_system.h"
 #include "pin_config.h"
 
-#define ARR_SIZE 1000
-
 #define TAG "Example"
-static uint8_t src_buf[ARR_SIZE];
-static uint8_t dst_buf[ARR_SIZE];
 
 void clearLED(void) {
   gpio_set_level(DIGI0, 0);
@@ -27,63 +21,161 @@ void clearLED(void) {
   gpio_set_level(G_LED, 0);
 }
 
-void async_memcpy_callback(void *arg) {
-  ESP_LOGI(TAG, "Async memcpy completed successfully");
+void setDigit(int digit) {
+  switch (digit) {
+  case 0:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 1);
+    gpio_set_level(E_LED, 1);
+    gpio_set_level(F_LED, 1);
+    gpio_set_level(G_LED, 0);
+    break;
+  case 1:
+    gpio_set_level(A_LED, 0);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 0);
+    gpio_set_level(E_LED, 0);
+    gpio_set_level(F_LED, 0);
+    gpio_set_level(G_LED, 0);
+    break;
+  case 2:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 0);
+    gpio_set_level(D_LED, 1);
+    gpio_set_level(E_LED, 1);
+    gpio_set_level(F_LED, 0);
+    gpio_set_level(G_LED, 1);
+    break;
+  case 3:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 1);
+    gpio_set_level(E_LED, 0);
+    gpio_set_level(F_LED, 0);
+    gpio_set_level(G_LED, 1);
+    break;
+  case 4:
+    gpio_set_level(A_LED, 0);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 0);
+    gpio_set_level(E_LED, 0);
+    gpio_set_level(F_LED, 1);
+    gpio_set_level(G_LED, 1);
+    break;
+  case 5:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 0);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 1);
+    gpio_set_level(E_LED, 0);
+    gpio_set_level(F_LED, 1);
+    gpio_set_level(G_LED, 1);
+    break;
+  case 6:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 0);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 1);
+    gpio_set_level(E_LED, 1);
+    gpio_set_level(F_LED, 1);
+    gpio_set_level(G_LED, 1);
+    break;
+  case 7:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 0);
+    gpio_set_level(E_LED, 0);
+    gpio_set_level(F_LED, 0);
+    gpio_set_level(G_LED, 0);
+    break;
+  case 8:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 1);
+    gpio_set_level(E_LED, 1);
+    gpio_set_level(F_LED, 1);
+    gpio_set_level(G_LED, 1);
+    break;
+  case 9:
+    gpio_set_level(A_LED, 1);
+    gpio_set_level(B_LED, 1);
+    gpio_set_level(C_LED, 1);
+    gpio_set_level(D_LED, 1);
+    gpio_set_level(E_LED, 0);
+    gpio_set_level(F_LED, 1);
+    gpio_set_level(G_LED, 1);
+    break;
+  default:
+    gpio_set_level(A_LED, 0);
+    gpio_set_level(B_LED, 0);
+    gpio_set_level(C_LED, 0);
+    gpio_set_level(D_LED, 0);
+    gpio_set_level(E_LED, 0);
+    gpio_set_level(F_LED, 0);
+    gpio_set_level(G_LED, 0);
+    break;
+  }
+}
+
+void setPlace(const int place) {
+  switch (place) {
+  case 0:
+    gpio_set_level(DIGI0, 1);
+    gpio_set_level(DIGI1, 0);
+    gpio_set_level(DIGI2, 0);
+    gpio_set_level(DIGI3, 0);
+    break;
+  case 1:
+    gpio_set_level(DIGI0, 0);
+    gpio_set_level(DIGI1, 1);
+    gpio_set_level(DIGI2, 0);
+    gpio_set_level(DIGI3, 0);
+    break;
+  case 2:
+    gpio_set_level(DIGI0, 0);
+    gpio_set_level(DIGI1, 0);
+    gpio_set_level(DIGI2, 1);
+    gpio_set_level(DIGI3, 0);
+    break;
+  case 3:
+    gpio_set_level(DIGI0, 0);
+    gpio_set_level(DIGI1, 0);
+    gpio_set_level(DIGI2, 0);
+    gpio_set_level(DIGI3, 1);
+    break;
+  default:
+    gpio_set_level(DIGI0, 0);
+    gpio_set_level(DIGI1, 0);
+    gpio_set_level(DIGI2, 0);
+    gpio_set_level(DIGI3, 0);
+    break;
+  }
 }
 
 void app_main(void) {
   // LED stuff
   initGPIOs();
   clearLED();
-  gpio_set_level(DIGI2, 1);
-  // int state = 0;
-
-  // DMA stuff
-  uint32_t *gpio_out = (uint32_t *)GPIO_OUT_REG;
-  uint32_t src_int = 0x00000610;
-  uint32_t dst_int = 0;
-
-  for (size_t i = 0; i < ARR_SIZE; i++) {
-    src_buf[i] = esp_random() % 256;
-  } // Copy source buffer to destination buffer asynchronously
-
-  // Initialize async memcpy driver
-  async_memcpy_config_t config = ASYNC_MEMCPY_DEFAULT_CONFIG();
-  async_memcpy_t driver = NULL;
-  ESP_ERROR_CHECK(esp_async_memcpy_install(
-      &config, &driver)); // install driver, return driver handle
-
-  // uint32_t *gpio_out_set = (uint32_t *)GPIO_OUT_W1TS_REG;
-  // uint32_t *gpio_out_clear = (uint32_t *)GPIO_OUT_W1TC_REG;
-
-  // int i = 0;
-  // *gpio_out |= (1 << C_LED);
+  setDigit(1);
+  setPlace(0);
+  int digit = 0;
+  int place = 0;
   while (1) {
-    // *gpio_out |= (1 << C_LED);
-    // printf("GPIO_OUT_REG [0x%08x] is 0x%08x\n", GPIO_OUT_REG, *gpio_out);
-    //
-    // printf("src[%d] = %d, dest[%d] = %d \n", i, src_buf[i], i, dst_buf[i]);
-    // i++;
-    // i %= ARR_SIZE;
-    // vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    // *gpio_out &= ~(1 << C_LED);
-    // printf("src[%d] = %d, dest[%d] = %d \n", i, src_buf[i], i, dst_buf[i]);
-    // i++;
-    // i %= ARR_SIZE;
-    // printf("GPIO_OUT_REG [0x%08x] is 0x%08x\n", GPIO_OUT_REG, *gpio_out);
-
-    esp_err_t ret = esp_async_memcpy(driver, gpio_out, &src_int,
-                                     2, NULL, NULL);
-
-    if (ret != ESP_OK) {
-      ESP_LOGE(TAG, "Async memcpy failed: %s", esp_err_to_name(ret));
-    }
-    else{
-      ESP_LOGI(TAG, "Async memcpy started");
-    }
-
-    printf("src = 0x%08x , dst = 0x%08x \n", src_int, *gpio_out);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    setDigit(digit);
+    setPlace(place);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    digit++;
+    // digit %= 10;
+    digit %= 4;
+    place++;
+    place %= 4;
   }
 }
