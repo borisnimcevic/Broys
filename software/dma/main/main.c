8eb61fdb01d63ff875e08d640f2affc9993b2b9b
Boris Nimcevic <borisnimcevic@gmail.com>
2023-03-06 16:05:03 +0100

    ISR working and digits are not flickering

--- software/dma/main/main.c
+++ software/dma/main/main.c
@@ -1,3 +1,5 @@
+#include "esp_attr.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -6,22 +8,40 @@
 
 #define TAG "Example"
 
+// #define TIMER_INTERVAL_US 1000000
+#define TIMER_INTERVAL_US    5000
+
+void IRAM_ATTR timer_isr(void *arg) {
+  static uint8_t digit = 0;
+  static uint8_t place = 0;
+
+  setDigit(digit);
+  setPlace(place);
+
+  digit++;
+  place++;
+
+  digit%=4;
+  place%=4;
+}
+
 void app_main(void) {
   initGPIOs();
   clearLED();
-  setDigit(1);
+  setDigit(0);
   setPlace(0);
-  int digit = 0;
-  uint8_t place = 0;
+
+  esp_timer_create_args_t timer_args = {.callback = timer_isr,
+                                        .arg = NULL,
+                                        .dispatch_method = ESP_TIMER_TASK,
+                                        .name = "my_timer"};
+
+  esp_timer_handle_t timer;
+
+  esp_timer_create(&timer_args, &timer);
+  esp_timer_start_periodic(timer, TIMER_INTERVAL_US);
 
   while (1) {
-    setDigit(digit);
-    setPlace(place);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    digit++;
-    // digit %= 10;
-    digit %= 4;
-    place++;
-    place %= 4;
   }
 }
